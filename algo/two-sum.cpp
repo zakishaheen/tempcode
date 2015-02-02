759811e67fcc36da49f1bdd9a1c7085c6431b901
Zaki Shaheen <zakishaheen@me.com    GIT_COMMITTER_NAME=zakishaheen    GIT_COMMITTER_EMAIL=zakishaheen@me.com>
2015-02-02 20:39:33 +0500

    added another solution

---
+++ algo/two-sum.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <deque>
+#include <cassert>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+		
+		//if you want to find an element in an array again and again, convert array into map like:
+		
+		map<int, vector<int> > hashmap;
+		
+		for(int i=0;i<numbers.size();i++){
+			vector<int> temp = hashmap[numbers[i]];
+			
+			if(temp.size()==0){
+				vector<int> n;
+				n.push_back(i);
+				hashmap[numbers[i]] = n;
+			}else{
+				temp.push_back(i);
+				hashmap[numbers[i]] = temp;
+			}	
+		}
+		
+		for(int i=0;i<numbers.size();i++){
+			
+			int remainder = target - numbers[i];
+			//notice that checking for existence directly will lead to an object added, hence use find or count
+			if(hashmap.find(remainder)!=hashmap.end()){
+				
+				vector<int> candidate = hashmap[remainder];
+				
+				if(hashmap.count(remainder) == 1 && candidate[0] == i){ continue; } 
+				
+				else if(hashmap.count(remainder) == 1 && candidate[0] != i){
+				
+					vector<int> result;
+					
+					if(i<candidate[0]){ result.push_back(i + 1); result.push_back(candidate[0] + 1);
+					}else{ result.push_back(candidate[0] + 1); result.push_back(i + 1);}
+																	return result;
+				}else{
+					
+					for(int k=1;k<candidate.size();k++){
+						if(i!=candidate[k]){
+							vector<int> result;
+							
+							if(i<k){ result.push_back(i + 1); result.push_back(candidate[k] + 1);
+							}else{ result.push_back(candidate[k] + 1); result.push_back(i + 1); }
+						}
+					}
+				}
+				
+			}
+			
+		}
+		
+    }
+};
+
+
+void test(int* arr,int size, int target, Solution s,int* expectedResult){
+	
+	vector<int> input (arr, arr + size);
+	
+	vector<int> output = s.twoSum(input, target);
+	
+	cout<<"Got:["<<output[0]<<","<<output[1]<<"]"<<endl;
+	assert(output[0] == expectedResult[0] && output[1] == expectedResult[1]);
+	
+}
+
+int main(int argc, char *argv[]) {
+	Solution s;
+		
+	test((int[]){2, 7, 11, 15}, 4, 9, s, (int[]){1,2});
+	test((int[]){2, 7, 11, 15}, 4, 13, s, (int[]){1,3});
+	test((int[]){-3, 4, 3, 90}, 4, 0, s, (int[]){1,3});	
+}
