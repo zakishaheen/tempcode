ce2f9f4e67763441cca24cfe728ea40bf7cf2682
Zaki Shaheen <zakishaheen@me.com    GIT_COMMITTER_NAME=zakishaheen    GIT_COMMITTER_EMAIL=zakishaheen@me.com>
2015-02-04 00:47:44 +0500

    added more solutions - longest-palindrome requires memoization

---
+++ algo/longest-palindrome.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <deque>
+
+using namespace std;
+
+class Solution {
+	string *memo;
+public:
+
+	string lpr(string s, int i, int j){
+		if(i==j) return s.substr(i,1);
+		
+		if(s[i]==s[j] && i+1==j){ return s.substr(i,2); }
+		
+		if(s[i]==s[j] && i+1!=j){
+			string value =  string(1,s[i]) + lpr(s, i+1, j-1) + string(1,s[j]);
+			
+			return value;
+		}
+		else{
+			string a = lpr(s, i+1, j);
+			string b = lpr(s, i, j-1);
+			
+			if(a.size()>b.size()){
+				return a;
+			}else{
+				return b;
+			}
+		}
+	}
+    string longestPalindrome(string s) {
+		return lpr(s,0,s.size()-1);
+	}
+};
+
+int main(int argc, char *argv[]) {
+	Solution s;
+	
+	cout<<s.longestPalindrome("a")<<endl;
+	cout<<s.longestPalindrome("aa")<<endl;
+	cout<<s.longestPalindrome("aaa")<<endl;
+	cout<<s.longestPalindrome("aaaa")<<endl;
+	cout<<s.longestPalindrome("aaaabaaaa")<<endl;
+	cout<<s.longestPalindrome("abaaaabaaaab")<<endl;
+	cout<<s.longestPalindrome("abaaaabaaaaba")<<endl;
+}
