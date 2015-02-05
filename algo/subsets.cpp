20503bcf5214aa8ad745abd378c5191682e601b0
‘zakishaheen’ <zakishaheen@me.com    GIT_COMMITTER_NAME=zakishaheen    GIT_COMMITTER_EMAIL=zakishaheen@me.com>
2015-02-05 20:33:09 +0500

    added more code

---
+++ algo/subsets.cpp
@@ -0,0 +1,33 @@
+/*
+Given a set of distinct integers, S, return all possible subsets.
+
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <stack>
+#include <deque>
+
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	
+}
