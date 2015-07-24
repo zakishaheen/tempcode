613a997b4882ded8d8119939258712c6ade6e1d7
‘zakishaheen’ <zakishaheen@me.com    GIT_COMMITTER_NAME=zakishaheen    GIT_COMMITTER_EMAIL=zakishaheen@me.com>
2015-07-24 19:01:27 +0500

    added staircase problem

---
+++ hacker-rank/practice/staircase.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//https://www.hackerrank.com/challenges/staircase
+
+int main() {
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+	
+	int N = 0;
+	cin>>N;
+	
+	for(int i = 0; i < N; i++){
+		
+		for(int j = N; j > i; j--){
+			cout<<" ";
+		}
+		for(int k = (N - 1) - i; k < N; k++){
+			cout<<"#";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
