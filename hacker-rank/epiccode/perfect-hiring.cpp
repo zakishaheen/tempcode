95e5bcd20e9fbc813d6ab2fc5e3895f0d16e5c84
debuggerman <zakishaheen@me.com    GIT_COMMITTER_NAME=zakishaheen    GIT_COMMITTER_EMAIL=zakishaheen@me.com>
2015-07-24 18:40:22 +0500

    added hackerrank practice problems

---
+++ hacker-rank/epiccode/perfect-hiring.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iostream> 
+
+using namespace std;
+
+
+int main() {
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+	
+	int N = 0;
+	long P = 0;
+	short X = 0;
+	
+	long *A = NULL;
+	
+	string params;
+	getline(cin, params);
+	
+	istringstream iss(params, istringstream::in);
+	
+	return 0;
+}
+
--- leetcode/leetcode-algo/excelsheet.cpp
+++ leetcode/leetcode-algo/excelsheet.cpp
@@ -7,16 +7,9 @@ class Solution {
 public:
     int titleToNumber(string s) {
 		int result = 0;
-		
-		int length=0;
-		
-		int multiplier = 0;
-		
-		
-		int index = 0;
-		
+				
 		for (int i=0;i<s.length();i++) {
-			result += multiplier; 
+
 			char x = s[((s.length()-1)-i)];
 			int col = (x - 'A') + 1;
 			int t = 1; for(int j=0;j<i;j++){ t*=26; }
